a7129275afda5fb346dc2fe1b9a73ace38e8e08b
rezamahesaa <reza.mahesa.azandi@gmail.com>
2024-08-13 15:26:23 +0700

    change to index.js on component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import EditRole from './components/RoleManagement/RoleEdit';
-import ShowUser from './components/RoleManagement/RoleManagementIndex';
+import ShowUser from './components/RoleManagement/index';
 
 function App() {
   
   return (
     <div className="App">
       <ShowUser />
-      <br/>
-      <br/>
-      <br/>
-      {/* <EditRole userId={50}/> */}
     </div>
   );
 }
